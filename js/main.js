cbbef335d867043397c2bfd71d405e85453a3904
Mohammed Abdellatief <mabdellatief.ext@orange.com>
2017-03-20 15:29:52 +0100

    creating test page

--- js/main.js
+++ js/main.js
@@ -1,63 +1,102 @@
 $(document).ready(function() {
 
+    (function() {
 
-    /**
-     * This part causes smooth scrolling using scrollto.js
-     * We target all a tags inside the nav, and apply the scrollto.js to it.
-     */
-        //=====================================================
-    $("nav a, .scrl_to").click(function(evn){
-        evn.preventDefault();
-        $('html,body').scrollTo(this.hash, this.hash);
-    });
+        // selectors
+        var line = $('#line'),
+            lineIn = $('#line1'),
+            lineOut = $('#line2'),
+            button = $('.btn'),
+            buttonUp = $('#btn1'),
+            buttonDown = $('#btn2'),
+            moreButton = $('.more_btn'),
+            drawerContainer = $('.bottom_drawer_container'),
+            percentage = $('.percentage')
+        ;
 
+        // line in elastic animation
+        function lineInAnimation(){
+            var tl = new TimelineMax();
 
+            tl.to(line,.25, {morphSVG:lineIn,ease: Circ.easeOut})
+              .to(line, 1, {morphSVG:line,ease: Elastic.easeOut.config(1, 0.3)})
+            ;
 
-    /**
-     * This part handles the highlighting functionality.
-     * We use the scroll functionality again, some array creation and
-     * manipulation, class adding and class removing, and conditional testing
-     */
-    //=====================================================
-    var aChildren = $("nav li").children(); // find the a children of the list items
-    var aArray = []; // create the empty aArray
-    for (var i=0; i < aChildren.length; i++) {
-        var aChild = aChildren[i];
-        var ahref = $(aChild).attr('href');
-        aArray.push(ahref);
-    } // this for loop fills the aArray with attribute href values
-
-    $(window).scroll(function(){
-        var windowPos = $(window).scrollTop()+2; // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
-        var docHeight = $(document).height();
-
-        for (var i=0; i < aArray.length; i++) {
-            var theID = aArray[i];
-            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
-            if (windowPos >= divPos && windowPos <= (divPos + divHeight)) {
-                $("a[href='" + theID + "']").addClass("active");
-            } else {
-                $("a[href='" + theID + "']").removeClass("active");
-            }
+            return tl ;
         }
 
-        if(windowPos + windowHeight == docHeight) {
-            if (!$("nav li:last-child a").hasClass("active")) {
-                var navActiveCurrent = $(".active").attr("href");
-                $("a[href='" + navActiveCurrent + "']").removeClass("active");
-                $("nav li:last-child a").addClass("active");
-            }
+
+        // line in elastic animation
+        function lineOutAnimation(){
+            var tl = new TimelineMax();
+
+            tl.to(line,.25, {morphSVG:lineOut,ease: Circ.easeOut})
+                .to(line, 1, {morphSVG:line,ease: Elastic.easeOut.config(1, 0.3)})
+            ;
+
+            return tl ;
         }
-    });
+
+
+
+        // button mouse Down elastic animation
+        function ButtonDownAnimation(){
+            var tl = new TimelineMax();
+
+            tl.to(buttonUp,.25, {morphSVG:buttonDown,ease: Circ.easeOut})
+            ;
+
+            return tl ;
+        }
+
+
+        // button mouse Up elastic animation
+        function ButtonUpAnimation(){
+            var tl = new TimelineMax();
+
+            tl.to(buttonUp,.7, {morphSVG:buttonUp,ease: Elastic.easeOut.config(1.5, 0.2)})
+            ;
+
+            return tl ;
+        }
+
+
+
+
+        // micro toggleClick jquery plugin
+        jQuery.fn.clickToggle = function(a,b) {
+            var ab = [b,a];
+            return this.on("click", function(){ ab[this._tog^=1].call(this); });
+        };
+
+        // click more button to toggle drawer
+        moreButton.clickToggle(function() {
+            lineOutAnimation();
+            drawerContainer.addClass('open');
+            percentage.addClass('shrink');
+            $(this).text('Less Details');
+
+        }, function() {
+            lineInAnimation();
+            drawerContainer.removeClass('open');
+            percentage.removeClass('shrink');
+            $(this).text('More Details');
+        });
+
+
+        // button click to wiggle
+        button.mousedown(function(){
+            ButtonDownAnimation();
+        });
+        button.mouseup(function(){
+            ButtonUpAnimation();
+        });
 
 
 
 
 
 
-
-
+    })();
 
 });
